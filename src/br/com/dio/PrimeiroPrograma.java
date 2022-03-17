c3cbdf54ca92b36f761ab77395e29dfe9b143bc2
RodrigoValentimAlves <rodrigo.valentim.alves@outlook.com>
2022-03-17 08:51:00 -0300

    criando class livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -8,9 +8,47 @@ public class PrimeiroPrograma {
 
         System.out.println(gato);
 
+        Livro livro1 = new Livro("O Problema dos 3 Corpos", "300");
+        System.out.println(livro1);
+
+
 
        /* int a = 5;
         int b = 3;
         System.out.println("Hello World!");*/
+    }
+}
+
+class Livro{
+    private String nome;
+    private Integer numpaginas;
+
+    public Livro(String nome, Integer numpaginas) {
+        this.nome = nome;
+        this.numpaginas =  numpaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumpaginas() {
+        return numpaginas;
+    }
+
+    public void setNumpaginas(Integer numpaginas) {
+        this.numpaginas = numpaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numpaginas=" + numpaginas +
+                '}';
     }
 }
